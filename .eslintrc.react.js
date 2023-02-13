0c928c6be4ed893702bc1443bb2a531dbec3bfc6
肖子立 <xiaozlb@digitalchina.com>
2023-02-13 14:08:12 +0800

    📦 NEW: add auto lint

---
+++ .eslintrc.react.js
@@ -0,0 +1,16 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ['plugin:react/recommended', 'plugin:react/jsx-runtime'],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {},
+};
